9ebe225748a7d0d2e52d5e1d3f3e10aeedef8614
Caz <caroline.portilla@gmail.com>
2020-03-01 00:10:27 +0800

    javascript beginnings

---
+++ script.js
@@ -0,0 +1,16 @@
+var timeEl = document.getElementById("time");
+
+var secondsLeft = 10;
+
+function setTime() {
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timeEl.textContent = secondsLeft;
+
+    if(secondsLeft === 0) {
+      clearInterval(timerInterval);
+      sendMessage();
+    }
+
+  }, 1000);
+}
